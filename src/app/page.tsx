db2bfa6ddc7068efee4ac3b4923dcf0a25ddb99b
максим <notolerances@yandex.ru>
2026-03-09 13:06:49 +0300

    `

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import './page.css';
 import styles from './page.module.css'
 import classnames from 'classnames'
 
-
 export default function Home() {
   return (
     <div className={styles.wrapper}>
