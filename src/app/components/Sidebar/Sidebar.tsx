87d371e49a1ff5671ff5c60828ad8fd6a56a5139
максим <notolerances@yandex.ru>
2026-03-12 22:55:38 +0300

    корректировки

--- src/app/components/Sidebar/Sidebar.tsx
+++ src/app/components/Sidebar/Sidebar.tsx
@@ -28,32 +28,26 @@ export default function Sidebar() {
                                 alt="day's playlist"
                                 width={250}
                                 height={170}
-                                priority
-                                style={{ width: '100%', height: 'auto' }}
                             />
                         </Link>
                     </div>
                     <div className={styles.sidebar__item}>
                         <Link className={styles.sidebar__link} href="#">
                             <Image
-                                className={styles.sidebar__img}
                                 src="/img/playlist02.png"
                                 alt="day's playlist"
                                 width={250}
                                 height={170}
-                                priority
                             />
                         </Link>
                     </div>
                     <div className={styles.sidebar__item}>
                         <Link className={styles.sidebar__link} href="#">
                             <Image
-                                className={styles.sidebar__img}
                                 src="/img/playlist03.png"
                                 alt="day's playlist"
                                 width={250}
                                 height={170}
-                                priority
                             />
                         </Link>
                     </div>
